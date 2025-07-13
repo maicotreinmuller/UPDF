26c0b948d54706b69b73dfc7c7631e8fa9e53008
maicotreinmuller <maicotreinmuller@gmail.com>
2025-07-13 20:06:00 -0300

    Fix PDF.js Worker with Next.js Configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/pdf.worker.min.js',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/javascript',
+          },
+        ],
+      },
+    ];
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
